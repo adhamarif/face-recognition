dfd3aaff6947c2f46811baa23fd9fbd5062a7fe4
Syahid Husein <92115178+syahidhusein@users.noreply.github.com>
2023-11-26 09:12:46 +0800

    Add files via upload
    - moved load_dataset.py to root for import purposes
    - added resize function because dataloader expects all images to be same size. Since cropped images are all different sizes, this is necessary.
    - added device.py to ensure all processes are done on the same device (either cpu or gpu)

---
+++ load_dataset.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from torchvision.io import read_image
+from torchvision.utils import make_grid
+from device import DEVICE
+
+LABEL_FILE = "C:\\Users\\ASUS\\datasets\\label.csv"
+IMAGE_FOLDER = "C:\\Users\\ASUS\\datasets\\face"
+
+resize_transform = transforms.Compose([
+    transforms.Resize((360, 360), antialias=True),
+])
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path).to(DEVICE)
+        label = self.img_labels.iloc[idx, 1]
+        # # Resize to target size, done here because all cropped faces are of different sizes
+        # image = resize_transform(image)
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+if __name__ == '__main__':
+
+    dataset = CustomImageDataset(LABEL_FILE, IMAGE_FOLDER)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    batch, label = dataloader.__iter__().__next__()
+        
+    grid = make_grid(batch, 8, padding=4).permute(1,2,0)
+
+    plt.imshow(grid.cpu())
+    plt.show()
