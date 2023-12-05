7a35a499e3db2a314a962504f8aa13a749847ccf
Adham Arif <adham.arif@gmail.com>
2023-12-05 19:28:56 +0100

    added transformation and change file source

--- autoencoder/load_dataset.py
+++ autoencoder/load_dataset.py
@@ -1,13 +1,15 @@
 import os
 import pandas as pd
+import torch
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import ToTensor
+from torchvision import transforms
+from device import DEVICE
 import matplotlib.pyplot as plt
 from torchvision.io import read_image
 from torchvision.utils import make_grid
 
-LABEL_FILE = r"C:\Users\Asus\Desktop\Project\face-recognition\datasets\label.csv"
-IMAGE_FOLDER = r"C:\Users\Asus\Desktop\Project\face-recognition\datasets\face"
+LABEL_FILE = r"D:\daisy_dataset\face_label.csv"
+IMAGE_FOLDER = r"D:\daisy_dataset\face"
 
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
@@ -21,7 +23,7 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
+        image = read_image(img_path).to(DEVICE)
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
@@ -30,12 +32,19 @@ class CustomImageDataset(Dataset):
         return image, label
 
 if __name__ == '__main__':
-    dataset = CustomImageDataset(LABEL_FILE, IMAGE_FOLDER)
+    # apply transformation to the images
+    transform = torch.nn.Sequential(
+        transforms.Resize((480, 480), antialias=True),
+        transforms.ConvertImageDtype(torch.float32)
+    )
+
+    # load the dataset
+    dataset = CustomImageDataset(LABEL_FILE, IMAGE_FOLDER, transform=transform)
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
     batch, label = dataloader.__iter__().__next__()
-        
-    grid = make_grid(batch, 8, padding=4).permute(1,2,0)
 
+    # plot the images grid    
+    grid = make_grid(batch, 8, padding=4).permute(1,2,0)
     plt.imshow(grid.cpu())
     plt.show()
