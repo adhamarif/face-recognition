decca4409e19c8661c330fbe2ae13ee0b03cb42e
Adham Arif <adham.arif@gmail.com>
2024-02-07 20:35:33 +0100

    add ae_model in the main

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ import time
 import argparse
 import gdown as gd
 import torch
-from network import NeuralNetwork
-from device import DEVICE
+from network.network import NeuralNetwork
+from autoencoder.device import DEVICE
 from transform import image_transform
 from PIL import Image
+from torchvision.transforms import transforms
+import torch.nn as nn
+from autoencoder.autoencoder_net import Network
 
+cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+cascade = cv2.CascadeClassifier(cascade_path)
+num_labels = 10
+#fr_net = NeuralNetwork(num_labels).to(DEVICE)
+confidence_threshold = 0.9
+
+ae_net = Network().to(DEVICE)
+
+PATH_TO_URL = {
+  #"fr_model_test.pt":  "https://drive.google.com/file/d/15qumR5MBFtoZpq-DO-si3nA91nLbVhS_/view",
+  "autoencoder_best_model.pth": "https://drive.google.com/file/d/1-P3wPTDgb2Xhw9NCnrfpUHNxXZl00_Jy/view?usp=sharing" #AE
+}
+
+# Define image transformation
+image_transform = transforms.Compose([
+    transforms.Resize((320, 320)),  # Resize the image
+    transforms.ToTensor()  # Convert PIL Image to PyTorch Tensor
+])
 
 def label_translator(pred_id):
     labels_dict = {
@@ -20,12 +41,6 @@ def label_translator(pred_id):
     for key,value in labels_dict.items():
         if pred_id == key : 
             return value
-
-cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-cascade = cv2.CascadeClassifier(cascade_path)
-num_labels = 10
-fr_net = NeuralNetwork(num_labels).to(DEVICE)
-confidence_threshold = 0.9
 
 def face_recognition(fr_model):
     # This function takes the face recognition model and runs the video footage
@@ -87,10 +102,74 @@ def face_recognition(fr_model):
     # Release the webcam and close all windows
     cap.release()
     cv2.destroyAllWindows()
+        
 
-PATH_TO_URL = {
-  "fr_model_test.pt":  "https://drive.google.com/file/d/15qumR5MBFtoZpq-DO-si3nA91nLbVhS_/view"
-}
+def ae_face_recognition(ae_model, loss_threshold=0.04):
+    # This function takes the face recognition model and runs the video footage
+    # It does the following in order:
+    # 1. Loads the model  from a file (if it exists) or finds the file online via a link. The model is saved as net_state_dict within a .pt checkpoint file.
+    # 2. Opens webcam and detects face using a cascade .xml file
+    # 3. If faces are found, it feeds them into the face recognition model to try predict a face.
+    # 4. Store the prediction as a variable. I will take it from here.
+    # Open the webcam
+    cap = cv2.VideoCapture(0)
+    cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+    cascade = cv2.CascadeClassifier(cascade_path)
+    mse_loss = nn.MSELoss()
+    while True:
+        # Read a frame from the webcam
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Error reading frame from webcam.")
+            break
+
+        # Convert the frame to grayscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Perform face detection using the cascade classifier
+        faces = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+        for (x, y, w, h) in faces:
+            # Extract the region of interest (ROI) containing the face
+            face_roi = frame[y:y + h, x:x + w]
+
+             # Convert the NumPy array to a PIL Image
+            face_pil_image = Image.fromarray(face_roi)
+
+            # Apply the transformations
+            transformed_image = image_transform(face_pil_image)
+
+            # Convert the transformed image to a PyTorch tensor
+            face_tensor = (transformed_image).unsqueeze(0).to(DEVICE)
+
+            # Perform face recognition using the loaded model
+            with torch.no_grad():
+                reconstructed = ae_model(face_tensor)
+            
+            loss = mse_loss(face_tensor, reconstructed)
+            # You can take it from here and process the prediction variable as needed
+
+            if loss < loss_threshold :
+                text = "Known Face"
+                color = (0, 255, 0)  # Green color
+            else:
+                text = "Unknown Face"
+                color = (0, 0, 255)  # Red color
+            # Draw a rectangle around the detected face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # Display the frame
+        cv2.imshow("Face Recognition", frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
  
 def load_model(net,model_name):
     if os.path.exists(model_name):
@@ -114,12 +193,17 @@ def load_model(net,model_name):
 # parser.add_argument("--model", required=True, help="Path to the face recognition model file")
 # args = parser.parse_args()
             
-# Load the face recognition model
-model = load_model(fr_net,"fr_model_test.pt")
+# # Load the face recognition model
+# model = load_model(fr_net,"fr_model_test.pt")
+# model.eval()
+# # model = load_model(fr_net,"fr_model_test.pt")
+
+# Load the autoencoder face recognition model
+model = load_model(ae_net,"autoencoder_best_model.pth")
 model.eval()
 # model = load_model(fr_net,"fr_model_test.pt")
             
 # Load the cascade classifier
 # Run face recognition with the specified model and cascade
-face_recognition(model)
+ae_face_recognition(model)
         
